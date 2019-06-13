dcfd54e8308012f73f6b00071cc102d8e0f176fa
stasesimchik <stasesimchik@mail.ru>
2019-06-13 15:03:58 +0300

    главная страница+адаптив
    главная страница+адаптив

--- js/main.js
+++ js/main.js
@@ -5,4 +5,13 @@ document.getElementById('menu-btn').onclick = function(){
   }else{
     document.getElementById('menu-list').style.display = 'block';
   }
-}
+};
+
+document.getElementById('cardBtn').onclick = function(){
+  var display = document.getElementById('cardList').style.display;
+  if(display == 'block'){
+    document.getElementById('cardList').style.display = 'none';
+  }else{
+    document.getElementById('cardList').style.display = 'block';
+  }
+};
